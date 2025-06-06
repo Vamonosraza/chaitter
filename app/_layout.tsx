f5c077ef693662ec1694e4e9ddf65d5411a856af
Vamonosraza <martinezjessy393@gmail.com>
2025-06-05 17:45:17 -0700

    Add initial iOS project files for Chaitter app
    - Created Info.plist with essential app configuration and URL schemes.
    - Added PrivacyInfo.xcprivacy to specify accessed APIs and privacy tracking settings.
    - Implemented SplashScreen.storyboard for the app's launch screen design.
    - Established Expo.plist for update configurations.
    - Added bridging header for Swift interoperability.
    - Created empty entitlements file for future use.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,24 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useRouter, useSegments } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+const useAuth = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+    };
+    checkAuth();
+  }, []);
+
+  return { isAuthenticated
+, setIsAuthenticated };
+};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -12,15 +26,42 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
+  const { isAuthenticated } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loaded) return;
+
+    const inAuthGroup = segments[0] === '(auth)';
+
+    if (isAuthenticated && inAuthGroup) {
+      // If the user is authenticated and in the auth group, redirect to home.
+      router.replace('/');
+    } else if (!isAuthenticated && !inAuthGroup) {
+      // If the user is not authenticated and not in the auth group, redirect to login.
+      router.replace('/(auth)/login');
+    }
+  }, [isAuthenticated, loaded, segments, router]);
+
+
+
   if (!loaded) {
     // Async font loading only occurs in development.
     return null;
   }
 
+  if ((isAuthenticated && segments[0] === '(auth)') || (!isAuthenticated && segments[0] !== '(auth)')) {
+    // If the user is authenticated and in the auth group, or not authenticated and not in the auth group, we don't render the layout.
+    return null;
+  }
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <StatusBar />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
